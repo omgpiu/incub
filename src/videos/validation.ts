4247decba407fa4d9cdd0193a84607bd4aee154c
omgpiu <dyba@list.ru>
2024-01-01 22:22:30 +0300

    feat: fixes

--- src/videos/validation.ts
+++ src/videos/validation.ts
@@ -47,7 +47,12 @@ export const putValidation = [
     .withMessage('minAgeRestriction field is required')
     .bail()
     .isNumeric()
-    .withMessage('minAgeRestriction must be a number'),
+    .withMessage('minAgeRestriction must be a number')
+    .bail()
+    .custom((value) => {
+      return value >= 1 && value <= 18;
+    })
+    .withMessage('minAgeRestriction must be between 1 and 18'),
   check('publicationDate')
     .notEmpty()
     .withMessage('publicationDate field is required')
