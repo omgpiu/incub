2e12a83a8dde1aa5b2079456bdf51ca38fb5879f
omgpiu <dyba@list.ru>
2024-01-01 20:21:11 +0300

    feat: add vercel.json

--- src/app.ts
+++ src/app.ts
@@ -34,11 +34,12 @@ app.use(function (req: Request, res: Response, next: NextFunction) {
 });
 
 // error handler
-const errorHandler: ErrorRequestHandler = (err, req, res) => {
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  console.log(res.locals.error, 'res.locals.error');
+  console.log(err, 'error');
   // render the error page
   res.status(err.status || 500);
   res.render('error');
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,8 @@ const router: Router = express.Router();
 
 /* GET home page. */
 router.get('/', (req: Request, res: Response) => {
+  console.log(req);
+  console.log('delpoyed');
   res.status(200).send('Hello World!');
 });
 
