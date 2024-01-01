d3c9e16c29876d11e97b6997a1dbba4428084062
omgpiu <dyba@list.ru>
2024-01-01 20:26:06 +0300

    feat: add vercel.json

--- src/db/videos.ts
+++ src/db/videos.ts
@@ -4,7 +4,7 @@ import { generateRandomResolution } from './generate';
 export const dbVideos = Array.from({ length: 10 }, (_, id) => ({
   id: id < 3 ? String(id) : faker.string.uuid(),
   title: faker.lorem.sentence(),
-  author: faker.name.firstName(),
+  author: faker.lorem.words({ min: 1, max: 2 }),
   canBeDownloaded: faker.datatype.boolean(),
   minAgeRestriction: faker.number.int({ min: 0, max: 21 }),
   createdAt: faker.date.past().toISOString(),
