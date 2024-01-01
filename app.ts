760b402cd880faa3f01e385b5e8d75e4b224be53
omgpiu <dyba@list.ru>
2024-01-01 16:43:20 +0300

    Initial commit

---
+++ app.ts
@@ -0,0 +1,43 @@
+import createError from 'http-errors';
+import express, { Request, Response, NextFunction, Application, ErrorRequestHandler } from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+
+import indexRouter from './routes/index'; // Ensure these are exported as modules
+import usersRouter from './routes/users';
+
+const app: Application = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req: Request, res: Response, next: NextFunction) {
+  next(createError(404));
+});
+
+// error handler
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+};
+
+app.use(errorHandler);
+
+export default app;
